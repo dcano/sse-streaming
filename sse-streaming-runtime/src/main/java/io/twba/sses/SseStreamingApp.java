00e4195f34c2e68caec7cbe1686c4b3e34c4ec0d
David Cano <dcanoh78@gmail.com>
2025-02-02 18:18:00 +0100

    Skeleton

---
+++ sse-streaming-runtime/src/main/java/io/twba/sses/SseStreamingApp.java
@@ -0,0 +1,13 @@
+package io.twba.sses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseStreamingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseStreamingApp.class, args);
+    }
+
+}
